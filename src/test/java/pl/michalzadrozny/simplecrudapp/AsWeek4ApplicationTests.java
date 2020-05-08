446156954b5d6b7a41e7351ca418f80856cc7ae7
Michał Zadrożny <mt.zadrozny@gmail.com>
2020-05-08 18:27:44 +0200

    Update

---
+++ src/test/java/pl/michalzadrozny/simplecrudapp/AsWeek4ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.michalzadrozny.simplecrudapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsWeek4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
